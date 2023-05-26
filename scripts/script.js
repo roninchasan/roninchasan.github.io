42f5575312f75be06b2755c5aafc62b796f874ec
Ronin Chasan <rcc263@cornell.edu>
2023-05-26 12:46:33 -0400

    update footer anim to scroll responsive

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,34 @@ function getCookie(cname) {
     }
     return "";
   }
+
+
+    const fadeInObjs = document.querySelectorAll(".toFadeIn");
+    const fadeInIcons = document.querySelectorAll(".toFadeInIcons");
+  
+    fadeInObjs.forEach(function(element) {
+        element.classList.add("hide-for-fade-in");
+  
+        window.addEventListener("scroll", function() {
+            let scroll = window.scrollY;
+  
+            if (scroll > element.offsetTop - 1100) {
+                setTimeout( () => {
+                    element.classList.add("fade-in-footer-menu");
+                }, 500)
+            }
+        });
+    });
+  
+  
+    fadeInIcons.forEach(function(element) {
+        element.classList.add("hide-for-fade-in");
+  
+        window.addEventListener("scroll", function() {
+            let scroll = window.scrollY;
+  
+            if (scroll > element.offsetTop - 1100) {
+                element.classList.add("fade-in-footer-icons");
+            }
+        });
+    });
